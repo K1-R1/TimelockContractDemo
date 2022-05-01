2d29fe9cc970e0afdcfeb24fe2cf99ad72619559
K1-R1 <kyranrawlinson@gmail.com>
2022-05-01 16:09:50 +0100

    Updated: TimeLock.sol, with getTxId function

--- TimeLock.sol
+++ TimeLock.sol
@@ -18,13 +18,31 @@ contract TimeLock {
         _;
     }
 
+    //Queue a valid transaction, that can only be executed after some delay
     function queue(
         address _target, //address to call
         uint256 _value, //value of ETH (wei) to send
         string calldata _func, //function on target to call
         bytes calldata _data, //data to pass to _func
         uint256 _timestamp //timestamp after which transaction can be executed
-    ) external onlyOwner {}
+    ) external onlyOwner {
+        //Create tx id
+        bytes32 txId = getTxId(_target, _value, _func, _data, _timestamp);
+        //check tx id unique
+        //check timestamp
+        //queue tx
+    }
+
+    //Get ID of transaction derived from inputs via hashing
+    function getTxId(
+        address _target, //address to call
+        uint256 _value, //value of ETH (wei) to send
+        string calldata _func, //function on target to call
+        bytes calldata _data, //data to pass to _func
+        uint256 _timestamp //timestamp after which transaction can be executed
+    ) public pure returns (bytes32 txId) {
+        return keccak256(abi.encode(_target, _value, _func, _data, _timestamp));
+    }
 
     function execute() external {}
 }
