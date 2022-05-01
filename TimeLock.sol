19cd4c147feda642bba722d355273b748bc01541
K1-R1 <kyranrawlinson@gmail.com>
2022-05-01 15:54:56 +0100

    Updated: TimeLock.sol, with queue function params

--- TimeLock.sol
+++ TimeLock.sol
@@ -3,7 +3,13 @@
 pragma solidity 0.8.13;
 
 contract TimeLock {
-    function queue() external {}
+    function queue(
+        address _target, //address to call
+        uint256 _value, //value of ETH (wei) to send
+        string calldata _func, //function on target to call
+        bytes calldata _data, //data to pass to _func
+        uint256 _timestamp //timestamp after which transaction can be executed
+    ) external {}
 
     function execute() external {}
 }
