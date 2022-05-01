d57cdd3e67a50ac7dbe34dd5b6cbfe925f9a37e7
K1-R1 <kyranrawlinson@gmail.com>
2022-05-01 16:00:57 +0100

    Updated: TimeLock.sol, with access control modifier

--- TimeLock.sol
+++ TimeLock.sol
@@ -3,13 +3,28 @@
 pragma solidity 0.8.13;
 
 contract TimeLock {
+    error NotOwnerError();
+
+    address public immutable OWNER;
+
+    constructor() {
+        OWNER = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != OWNER) {
+            revert NotOwnerError();
+        }
+        _;
+    }
+
     function queue(
         address _target, //address to call
         uint256 _value, //value of ETH (wei) to send
         string calldata _func, //function on target to call
         bytes calldata _data, //data to pass to _func
         uint256 _timestamp //timestamp after which transaction can be executed
-    ) external {}
+    ) external onlyOwner {}
 
     function execute() external {}
 }
